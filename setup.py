b23f113f69efb4e2a7cb7d90716b9c6cb81bd498
ning <ningyuan.sg@gmail.com>
2023-04-03 08:19:53 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+# Meta information
+version = "0.0.0-snapshot1"
+dirname = os.path.dirname(__file__)
+
+setup(
+    # Basic info
+    name="nu",
+    version=version,
+    author="ning",
+    author_email="ningyuan.sg@gmail.com",
+    url="https://github.com/ning-y/nu",
+    description="Assorted utilities for ning's personal use.",
+    long_description="Assorted utilities for ning's personal use.",
+    classifiers=[
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Programming Language :: Python",
+    ],
+
+    # Packages and depencies
+    py_modules=["nu"],
+    install_requires=[
+        "xdg",
+    ],
+
+    # Scripts
+    entry_points={
+        "console_scripts": [
+            "nu = nu.__main__:main"],
+    },
+)
